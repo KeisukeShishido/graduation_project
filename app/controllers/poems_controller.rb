7b2542edd74cb9af38b8bab4c87f2c9cdb506b45
KeisukeShishido <aya.kotori0723@gmail.com>
2025-05-04 22:53:06 +0900

    fix：リントチェック修正

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -22,33 +22,33 @@ class PoemsController < ApplicationController
     @poem = current_user.poems.build(poem_params)
 
     if @poem.save
-        redirect_to @poem, notice: t('views.defaults.flash_message.created', item: Poem.model_name.human)
+        redirect_to @poem, notice: t("views.defaults.flash_message.created", item: Poem.model_name.human)
     else
-        flash.now[:alert] = t('views.defaults.flash_message.not_created', item: Poem.model_name.human)
+        flash.now[:alert] = t("views.defaults.flash_message.not_created", item: Poem.model_name.human)
         render :new, status: :unprocessable_entity
     end
   end
 
   def update
     if @poem.update(poem_params)
-        redirect_to @poem, notice: t('views.defaults.flash_message.updated', item: Poem.model_name.human)
+        redirect_to @poem, notice: t("views.defaults.flash_message.updated", item: Poem.model_name.human)
     else
-        flash.now[:alert] = t('views.defaults.flash_message.not_updated', item: Poem.model_name.human)
+        flash.now[:alert] = t("views.defaults.flash_message.not_updated", item: Poem.model_name.human)
         render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @poem.destroy!
-    redirect_to poems_path, notice: t('views.defaults.flash_message.destroyed', item: Poem.model_name.human)
+    redirect_to poems_path, notice: t("views.defaults.flash_message.destroyed", item: Poem.model_name.human)
   end
 
   private
     def set_poem
       @poem = current_user.poems.find_by(id: params[:id])
       unless @poem
-        redirect_to poems_path, alert: t('views.defaults.flash_message.not_found', item: Poem.model_name.human)
-    end
+        redirect_to poems_path, alert: t("views.defaults.flash_message.not_found", item: Poem.model_name.human)
+      end
   end
 
     def poem_params
@@ -58,5 +58,4 @@ class PoemsController < ApplicationController
     def authorize_user!
       redirect_to poems_path, alert: "権限がありません" unless @poem.user == current_user
     end
-
 end
