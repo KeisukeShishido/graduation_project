cd45c07dc8792608517fcd19a3253872f34a1c81
KeisukeShishido <aya.kotori0723@gmail.com>
2025-05-04 22:36:14 +0900

    add：フラッシュメッセージのデザイン部分をhelperに作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_message_color(type)
+    case type.to_sym
+    when :notice
+      "bg-green-300 text-green-600 border border-green-500"
+    when :alert
+      "bg-red-200 text-red-600 border border-red-500"
+    else
+      "bg-gray-200 text-gray-600 border border-gray-400"
+    end
+  end
 end
