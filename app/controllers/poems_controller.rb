30bffbac88ddeaee599d0860b0a1abd9d802fbf3
KeisukeShishido <aya.kotori0723@gmail.com>
2025-05-04 22:33:34 +0900

    add：フラッシュメッセージ用にコントローラーを修正

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,70 +3,53 @@ class PoemsController < ApplicationController
   before_action :set_poem, only: %i[ edit update destroy ]
   before_action :authorize_user!, only: [ :edit, :update, :destroy ]
 
-  # GET /poems or /poems.json
   def index
     @poems = Poem.order(created_at: :desc)
   end
 
-  # GET /poems/1 or /poems/1.json
   def show
     @poem = Poem.find(params[:id])
   end
 
-  # GET /poems/new
   def new
     @poem = Poem.new
   end
 
-  # GET /poems/1/edit
   def edit
   end
 
-  # POST /poems or /poems.json
   def create
     @poem = current_user.poems.build(poem_params)
 
-    respond_to do |format|
-      if @poem.save
-        format.html { redirect_to @poem, notice: "Poem was successfully created." }
-        format.json { render :show, status: :created, location: @poem }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @poem.errors, status: :unprocessable_entity }
-      end
+    if @poem.save
+        redirect_to @poem, notice: t('views.defaults.flash_message.created', item: Poem.model_name.human)
+    else
+        flash.now[:alert] = t('views.defaults.flash_message.not_created', item: Poem.model_name.human)
+        render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /poems/1 or /poems/1.json
   def update
-    respond_to do |format|
-      if @poem.update(poem_params)
-        format.html { redirect_to @poem, notice: "Poem was successfully updated." }
-        format.json { render :show, status: :ok, location: @poem }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @poem.errors, status: :unprocessable_entity }
-      end
+    if @poem.update(poem_params)
+        redirect_to @poem, notice: t('views.defaults.flash_message.updated', item: Poem.model_name.human)
+    else
+        flash.now[:alert] = t('views.defaults.flash_message.not_updated', item: Poem.model_name.human)
+        render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /poems/1 or /poems/1.json
   def destroy
     @poem.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to poems_path, status: :see_other, notice: "Poem was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to poems_path, notice: t('views.defaults.flash_message.destroyed', item: Poem.model_name.human)
   end
 
   private
     def set_poem
       @poem = current_user.poems.find_by(id: params[:id])
-      if @poem.nil?
-        redirect_to poems_path, alert: "詩が見つかりません。別の詩を確認してください。"
-      end
+      unless @poem
+        redirect_to poems_path, alert: t('views.defaults.flash_message.not_found', item: Poem.model_name.human)
     end
+  end
 
     def poem_params
       params.require(:poem).permit(:title, :aroma, :location, :content)
@@ -75,4 +58,5 @@ class PoemsController < ApplicationController
     def authorize_user!
       redirect_to poems_path, alert: "権限がありません" unless @poem.user == current_user
     end
+
 end
